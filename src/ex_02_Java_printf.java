42e98c80613e999dbfc9a2a81a73c69a19e70919
gowdhamn <gowdhamn@gmail.com>
2025-12-21 19:58:58 +0530

    Initial commit

---
+++ src/ex_02_Java_printf.java
@@ -0,0 +1,21 @@
+public class ex_02_Java_printf {
+
+   public static void main() {
+
+       int tables= 9;
+
+       System.out.printf("Multiplication table: %d * 1 = %d%n",tables,tables*1);
+       System.out.printf("Multiplication table: %d * 2 = %d%n",tables,tables*2);
+       System.out.printf("Multiplication table: %d * 3 = %d%n",tables,tables*3);
+       System.out.printf("Multiplication table: %d * 4 = %d%n",tables,tables*4);
+       System.out.printf("Multiplication table: %d * 5 = %d%n",tables,tables*5);
+       System.out.printf("Multiplication table: %d * 6 = %d%n",tables,tables*6);
+       System.out.printf("Multiplication table: %d * 7 = %d%n",tables,tables*7);
+       System.out.printf("Multiplication table: %d * 8 = %d%n",tables,tables*8);
+       System.out.printf("Multiplication table: %d * 9 = %d%n",tables,tables*9);
+       System.out.printf("Multiplication table: %d * 10 = %d%n",tables,tables*10);
+       System.out.printf("Multiplication table: %d * 11 = %d%n",tables,tables*11);
+       System.out.printf("Multiplication table: %d * 12 = %d%n",tables,tables*12);
+
+    }
+}
