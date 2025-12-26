196bcc93470534524348591107f31c3268696499
gowdhamn <gowdhamn@gmail.com>
2025-12-26 17:05:37 +0530

    arithmetic_26th_dec

---
+++ src/Lab002_exercises/Arithmetic_operations.java
@@ -0,0 +1,13 @@
+package Lab002_exercises;
+
+public class Arithmetic_operations {
+    static void main() {
+        int x=20,y=10;
+
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+        System.out.println(x*y);
+    }
+}
