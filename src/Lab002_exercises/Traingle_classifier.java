b8dc3690d093061f44f86817575c3550893aeee6
gowdhamn <gowdhamn@gmail.com>
2025-12-29 21:51:38 +0530

    Triangle_classifier_27th_dec_exercise

--- src/Lab002_exercises/Traingle_classifier.java
+++ src/Lab002_exercises/Traingle_classifier.java
@@ -1,14 +1,17 @@
 package Lab002_exercises;
 
+import java.util.Scanner;
+
 public class Traingle_classifier {
     static void main(String[] args) {
 
-      /*  int x= Integer.parseInt(args[0]);
-        int y=Integer.parseInt(args[1]);
-        int z = Integer.parseInt(args[2]);
-       */
-
-        int x=20,y=50,z=20;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value for x:");
+        int x= scanner.nextInt();
+        System.out.println("Enter the value for y:");
+        int y= scanner.nextInt();
+        System.out.println("Enter the value for z:");
+        int z= scanner.nextInt();
 
         if (x == y && y== z && x==z)
         {
@@ -22,5 +25,6 @@ public class Traingle_classifier {
         {
             System.out.println("It is an scalene traingle");
         }
+        scanner.close();
     }
 }
---
+++ src/Lab002_exercises/unary_operators.java
@@ -0,0 +1,11 @@
+package Lab002_exercises;
+
+public class unary_operators {
+
+    static void main() {
+
+        int a =10;
+        System.out.println(a++ + a);
+        System.out.println(a);
+    }
+}
